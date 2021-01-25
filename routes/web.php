1d4bf78e5727da094fe54667cee1a2243cadb328
ahmadhilmi420 <ahmadhilmi420@gmail.com>
2021-01-25 18:25:36 +0700

    edit hilmi

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-//ini edit sama zakharia
-// ini ediat sama Hilmi
-// edit baru zakharia   
